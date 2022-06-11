fe63c03fabfa12538cdd7b5b98b6062b60aaadc2
Shields <mshields@nrel.gov>
2022-06-10 22:11:56 -0600

    Loop through different scenarios

--- run_analysis.py
+++ run_analysis.py
@@ -12,166 +12,227 @@ from ORBIT.core.library import initialize_library
 from CORAL import SharedLibrary, GlobalManager, Pipeline
 from CORAL.utils import get_installed_capacity_by
 
+# name scenarios
+scenario_names = [
+    '_unlimited_akt',
+    '_unlimited_no_akt',
+    '_limitWTIV',
+    '_limitWTIV',
+    '_delayedPort',
+    # 1 year construction delays on SBMT, AKT, Salem, New London
+    '_delayedPort_limitWTIV',
+    '_limitFeeder'
+]
 
+pipeline_list = [
+    "east_coast_analysis/pipeline_OWMR2022_akt.csv",
+    "east_coast_analysis/pipeline_OWMR2022.csv",
+    "east_coast_analysis/pipeline_OWMR2022_akt.csv",
+    "east_coast_analysis/pipeline_OWMR2022_akt.csv",
+    "east_coast_analysis/pipeline_OWMR2022_delays.csv",
+    "east_coast_analysis/pipeline_OWMR2022_delays.csv",
+    "east_coast_analysis/pipeline_OWMR2022_akt.csv",
+]
+#### Define initial port and vessel configuration  (all the same for now)
+allocations_list = [
+    {"wtiv": ('example_wtiv', 1),
+    "feeder": ('example_feeder', 10),
+    "port": [
+        ('new_bedford', 1),
+        ('new_london', 1),
+        ('njwp', 1),
+        ('sbmt', 1),
+        ('portsmouth', 1),
+        ('salem', 1),
+        ('tradepoint', 1),
+        ('akt', 1)
+    ],
+},
+] * len(pipeline_list) + [
+    {"wtiv": ('example_wtiv', 1),
+    "feeder": ('example_feeder', 2),
+    "port": [
+        ('new_bedford', 1),
+        ('new_london', 1),
+        ('njwp', 1),
+        ('sbmt', 1),
+        ('portsmouth', 1),
+        ('salem', 1),
+        ('tradepoint', 1),
+        ('akt', 1)
+    ],
+},
+]
+
+future_resources_list = [
+[
+    ['wtiv', 'example_wtiv', [dt.datetime(2025, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2026, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2029, 1, 1)]],
+    ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
+],  # _unlimited_akt
+[
+    ['wtiv', 'example_wtiv', [dt.datetime(2025, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2026, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2029, 1, 1)]],
+    ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
+],  # _unlimited_no_akt
+[
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
+],  # _limitWTIV
+[
+    ['wtiv', 'example_wtiv', [dt.datetime(2025, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
+],  # _limitWTIV
+[
+    ['wtiv', 'example_wtiv', [dt.datetime(2025, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2026, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2029, 1, 1)]],
+    ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
+],  # _delayedPort
+[
+    ['wtiv', 'example_wtiv', [dt.datetime(2025, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
+],  # _delayedPort_limitWTIV
+[
+    ['wtiv', 'example_wtiv', [dt.datetime(2025, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2026, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
+    ['wtiv', 'example_wtiv', [dt.datetime(2029, 1, 1)]],
+    ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
+    ['feeder', 'example_feeder', [dt.datetime(2024, 1, 1)]],
+    ['feeder', 'example_feeder', [dt.datetime(2025, 1, 1)]],
+    ['feeder', 'example_feeder', [dt.datetime(2026, 1, 1)]],
+    ['feeder', 'example_feeder', [dt.datetime(2027, 1, 1)]],
+    ['feeder', 'example_feeder', [dt.datetime(2028, 1, 1)]],
+    ['feeder', 'example_feeder', [dt.datetime(2029, 1, 1)]],
+    ['feeder', 'example_feeder', [dt.datetime(2030, 1, 1)],]
+],  # _limitFeeder
+]
 
 if __name__ == '__main__':
     initialize_library(os.path.join(os.getcwd(), "east_coast_analysis/library"))
 
-    projects = os.path.join(os.getcwd(), "east_coast_analysis/pipeline_OWMR2022.csv")
-    base = os.path.join(os.getcwd(), "east_coast_analysis/base.yaml")
-    pipeline = Pipeline(projects, base, regional_ports=False)
-
-    # weather_no_whales = pd.read_csv("east_coast_analysis/library/weather/vineyard_wind_weather_1983_2017_orbit.csv", parse_dates=['datetime']).set_index('datetime')
     weather = pd.read_csv("east_coast_analysis/library/weather/vineyard_wind_repr_with_whales.csv", parse_dates=["datetime"]).set_index("datetime")
 
     library_path = os.path.join(os.getcwd(), "east_coast_analysis/library")
 
-    #### Define initial port and vessel configuration
-    allocations = {
-        "wtiv": ('example_wtiv', 1),
-        "feeder": ('example_feeder', 10),
-        "port": [
-            ('new_bedford', 1),
-            ('new_london', 1),
-            ('njwp', 1),
-            ('sbmt', 1),
-            ('portsmouth', 1),
-            ('salem', 1),
-            ('tradepoint', 1),
-            ('akt', 1)
-        ],
-    }
+    for allocations, future_resources, pipeline, name in zip(allocations_list, future_resources_list, pipeline_list, scenario_names):
 
-    num_wtiv = len(allocations['wtiv'])
-    num_port = len(allocations['port'])
+        projects = os.path.join(os.getcwd(), pipeline)
+        base = os.path.join(os.getcwd(), "east_coast_analysis/base.yaml")
+        pipeline = Pipeline(projects, base, regional_ports=False)
 
-    manager = GlobalManager(pipeline.configs, allocations, library_path=library_path, weather=weather)
+        num_wtiv = len(allocations['wtiv'])
+        num_port = len(allocations['port'])
 
-    # ASsumptions
-    # Test
-    # New Bedford, PMT ready now.  Might be able to marshall 2 out of PMT
-    # NJWP ready in 2023 and expanded in 2025
-    # State Pier ready in 2023
-    # SBMT ready in 2024 (guess)
-    # Salem ready in 2027 for Commonwealth Wind (call it New Bedford in model)
-    # CORAL seems to need to initialize ports or runs into key error, so setting SBMT, NLSP, and NJWP (phase 1) = 1 at start
-    #######
-    # 1 vessel (Charybdis) ready at beginning
-    # MAersk ready in 2025
-    # Additional WTIVs online in 2026 (Keppel) and 2x 2028 (Philly, VT Halter) and 1x 2029
+        manager = GlobalManager(pipeline.configs, allocations, library_path=library_path, weather=weather)
+
+        new_wtiv = [1 for fr in future_resources if 'wtiv' in fr]
+        new_ports = [1 for fr in future_resources if 'port' in fr]
+
+        total_wtiv = np.sum([num_wtiv]+new_wtiv)
+        total_ports = np.sum([num_port]+new_ports)
+
+        fig_name = str(total_wtiv)+'wtiv_'+str(total_ports)+'ports_'+name
 
 
-    # Notes:
-    # Just adding a port doens't do much.  Also need WTIVs.
+        for f in future_resources:
+            manager.add_future_resources(f[0], f[1], f[2])
 
-    future_resources = [
-        # ['wtiv', 'example_wtiv', [dt.datetime(2023, 5, 1)]],
-        ['wtiv', 'example_wtiv', [dt.datetime(2025, 1, 1)]],
-        ['wtiv', 'example_wtiv', [dt.datetime(2026, 1, 1)]],
-        ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
-        ['wtiv', 'example_wtiv', [dt.datetime(2028, 1, 1)]],
-        ['wtiv', 'example_wtiv', [dt.datetime(2029, 1, 1)]],
-        # ['port', 'new_bedford', [dt.datetime(2027, 1, 1)]],  # (=SAlem)
-        # ['port', 'njwp', [dt.datetime(2023, 5, 1)]],
-        # ['port', 'new_london', [dt.datetime(2023, 5, 1)]],
-        # ['port', 'sbmt', [dt.datetime(2027, 1, 1)]],
-        ['port', 'njwp', [dt.datetime(2025, 1, 1)]],
-    ]
+        # Run simulation with future resources
+        manager.run()
+        print('Simulation complete')
 
-    new_wtiv = [1 for fr in future_resources if 'wtiv' in fr]
-    new_ports = [1 for fr in future_resources if 'port' in fr]
+        # Postprocessing
+        df = pd.DataFrame(manager.logs).iloc[::-1]
+        df = df.reset_index(drop=True).reset_index()
 
-    total_wtiv = np.sum([num_wtiv]+new_wtiv)
-    total_ports = np.sum([num_port]+new_ports)
+        # Save csv
+        csv_name = fig_name + '_data.csv'
+        df.to_csv(csv_name)
 
-    fig_name = str(total_wtiv)+'wtiv_'+str(total_ports)+'ports'
+        # Assign ports to projects
+        port_map = pipeline.projects[["name", "associated_port", "capacity"]].set_index("name").to_dict()['associated_port']
+        df['port'] = [port_map[name] for name in df['name']]
 
+        capacity_map = pipeline.projects[["name", "capacity"]].set_index("name").to_dict()['capacity']
+        df['capacity'] = [capacity_map[name] for name in df['name']]
 
-    for f in future_resources:
-        manager.add_future_resources(f[0], f[1], f[2])
+        # Plot results
+        fig = plt.figure(figsize=(10, 6), dpi=300)
+        ax = fig.add_subplot(111)
 
-    # Run simulation with future resources
-    manager.run()
-    print('Simulation complete')
+        df["Date Finished"].plot(kind="barh", ax=ax, zorder=4, label="Project Installation", color="#b1b1b1")
+        df["Date Started"].plot(kind="barh", color="#e9e9e9", ax=ax, zorder=4, label="Project Delay", hatch="////", linewidth=0.5)
+        df["Date Initialized"].plot(kind='barh', ax=ax, zorder=4, label="__nolabel__", color='w')
 
-    # Postprocessing
-    df = pd.DataFrame(manager.logs).iloc[::-1]
-    df = df.reset_index(drop=True).reset_index()
+        # Plot formatting
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+        _ = ax.set_yticklabels(df['name'])
+        plt.yticks(fontsize=6)
+        ax.legend()
+        ax.set_xlim(manager._start - dt.timedelta(days=30), dt.date(2036, 6, 1) + dt.timedelta(days=30))#df["Date Finished"].max() + dt.timedelta(days=30))
 
-    # Save csv
-    df.to_csv('east_coast_analysis/figures/sc_roadmap_gaps/east_coast_simulation.csv')
+        ax.axvline(dt.date(2031, 1, 1), lw=0.5, ls="--", color="k", zorder=6)
+        installed_capacity = get_installed_capacity_by(df, 2031)
+        ax.text(x=dt.date(2031, 4, 1), y=20, s=f"Capacity installed \nby end of 2030: \n{installed_capacity/1000:,.3} GW", fontsize=20)
 
-    # Assign ports to projects
-    port_map = pipeline.projects[["name", "associated_port", "capacity"]].set_index("name").to_dict()['associated_port']
-    df['port'] = [port_map[name] for name in df['name']]
+        fig.subplots_adjust(left=0.25)
 
-    capacity_map = pipeline.projects[["name", "capacity"]].set_index("name").to_dict()['capacity']
-    df['capacity'] = [capacity_map[name] for name in df['name']]
+        fname_d = 'east_coast_analysis/figures/sc_roadmap_gaps/deployment_'+fig_name+'.png'
+        fig.savefig(fname_d, dpi=300)
 
-    # Plot results
-    fig = plt.figure(figsize=(10, 6), dpi=300)
-    ax = fig.add_subplot(111)
+        ### Annual throughput
 
-    df["Date Finished"].plot(kind="barh", ax=ax, zorder=4, label="Project Installation", color="#b1b1b1")
-    df["Date Started"].plot(kind="barh", color="#e9e9e9", ax=ax, zorder=4, label="Project Delay", hatch="////", linewidth=0.5)
-    df["Date Initialized"].plot(kind='barh', ax=ax, zorder=4, label="__nolabel__", color='w')
+        res = []
 
-    # Plot formatting
-    ax.set_xlabel("")
-    ax.set_ylabel("")
-    _ = ax.set_yticklabels(df['name'])
-    plt.yticks(fontsize=6)
-    ax.legend()
-    ax.set_xlim(manager._start - dt.timedelta(days=30), dt.date(2036, 6, 1) + dt.timedelta(days=30))#df["Date Finished"].max() + dt.timedelta(days=30))
+        for _, project in df.iterrows():
 
-    ax.axvline(dt.date(2031, 1, 1), lw=0.5, ls="--", color="k", zorder=6)
-    installed_capacity = get_installed_capacity_by(df, 2031)
-    ax.text(x=dt.date(2031, 4, 1), y=20, s=f"Capacity installed \nby end of 2030: \n{installed_capacity/1000:,.3} GW", fontsize=20)
+            if project["Date Finished"].year == project["Date Started"].year:
+                res.append((project["Date Finished"].year, project["port"], project["capacity"]))
 
-    fig.subplots_adjust(left=0.25)
+            else:
 
-    fname_d = 'east_coast_analysis/figures/sc_roadmap_gaps/deployment_'+fig_name+'.png'
-    fig.savefig(fname_d, dpi=300)
+                perc = (project["Date Finished"].date() - dt.date(project["Date Finished"].year, 1, 1)) /\
+                    (project["Date Finished"] - project["Date Started"])
 
-    ### Annual throughput
+                res.append((project["Date Finished"].year, project["port"], perc * project["capacity"]))
+                res.append((project["Date Started"].year, project["port"], (1 - perc) * project["capacity"]))
 
-    res = []
+        throughput = pd.DataFrame(res, columns=["year", "port", "capacity"]).pivot_table(
+            index=["year"],
+            columns=["port"],
+            aggfunc="sum",
+            fill_value=0.
+        )["capacity"]
 
-    for _, project in df.iterrows():
+        fig = plt.figure(figsize=(6, 4), dpi=200)
+        ax = fig.add_subplot(111)
 
-        if project["Date Finished"].year == project["Date Started"].year:
-            res.append((project["Date Finished"].year, project["port"], project["capacity"]))
+        throughput.plot(ax=ax, lw=0.7)
 
-        else:
+        ax.set_ylim(0, 2000)
 
-            perc = (project["Date Finished"].date() - dt.date(project["Date Finished"].year, 1, 1)) /\
-                (project["Date Finished"] - project["Date Started"])
+        ax.set_ylabel("Annual Capacity Throughput (MW)")
+        ax.set_xlabel("")
 
-            res.append((project["Date Finished"].year, project["port"], perc * project["capacity"]))
-            res.append((project["Date Started"].year, project["port"], (1 - perc) * project["capacity"]))
+        plt.xticks(rotation=0, fontsize=6)
+        plt.yticks(fontsize=6)
 
-    throughput = pd.DataFrame(res, columns=["year", "port", "capacity"]).pivot_table(
-        index=["year"],
-        columns=["port"],
-        aggfunc="sum",
-        fill_value=0.
-    )["capacity"]
+        ax.legend(fontsize=6, ncol=5)
+        fname_t = 'east_coast_analysis/figures/sc_roadmap_gaps/throughput_'+fig_name+'.png'
+        fig.savefig(fname_t, dpi=300)
 
-    fig = plt.figure(figsize=(6, 4), dpi=200)
-    ax = fig.add_subplot(111)
-
-    throughput.plot(ax=ax, lw=0.7)
-
-    ax.set_ylim(0, 2000)
-
-    ax.set_ylabel("Annual Capacity Throughput (MW)")
-    ax.set_xlabel("")
-
-    plt.xticks(rotation=0, fontsize=6)
-    plt.yticks(fontsize=6)
-
-    ax.legend(fontsize=6, ncol=5)
-    fname_t = 'east_coast_analysis/figures/sc_roadmap_gaps/throughput_'+fig_name+'.png'
-    fig.savefig(fname_t, dpi=300)
-
-    # plt.show()
+        # plt.show()
